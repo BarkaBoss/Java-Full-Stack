b99e03bdd6cc2f1ca1bbd988b13476032b54a430
Barka Tatama <bossmajor.biii@gmail.com>
2024-05-03 23:49:12 +0100

    Playing with the string class

--- oop1/LandAnimal.java
+++ oop1/LandAnimal.java
@@ -8,8 +8,11 @@ public class LandAnimal extends Animal{
     }
 
     public static void main(String[] args) {
+        Animal chimp = new LandAnimal();
         LandAnimal monkey = new LandAnimal();
         monkey.name = "Monkey";
+        chimp.name = "Champa";
         System.out.println(monkey.name+" "+monkey.move());
+        System.out.println(chimp.name+" "+monkey.move());
     }
 }
--- string/StringDemo.java
+++ string/StringDemo.java
@@ -1,9 +1,22 @@
 package string;
 
+import java.util.Scanner;
+
 public class StringDemo {
 
     public static void main(String[] args) {
-        String str = "Moose";
+        StringDemo demo = new StringDemo();
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+
+        demo.reverseString(str);
+        String repStr = str.replace("c", "v");
+        System.out.println(repStr);
+    }
+
+
+    String reverseString(String string){
+        String str = string;
         System.out.println(str.codePointCount(1, 4));
         StringBuilder rev = new StringBuilder();
 
@@ -12,8 +25,6 @@ public class StringDemo {
         }
         System.out.println(rev);
 
-        String repStr = str.replace("Mo", "Ch");
-        System.out.println(repStr);
+        return rev.toString();
     }
-
 }

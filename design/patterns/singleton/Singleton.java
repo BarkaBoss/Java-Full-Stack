42e5667e554f519f70c3d331b588f78d1fed9d3c
Barka Tatama <bossmajor.biii@gmail.com>
2024-07-22 09:40:14 +0100

    Made default constructor private

--- design/patterns/singleton/Singleton.java
+++ design/patterns/singleton/Singleton.java
@@ -1,6 +1,7 @@
 package design.patterns.singleton;
 
 public class Singleton {
+    private Singleton() {}
     private static Singleton singleton = new Singleton();
 
     public static Singleton getSingleton() {
